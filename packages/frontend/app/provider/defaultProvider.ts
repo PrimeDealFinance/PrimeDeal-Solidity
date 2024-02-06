18747f817735ad4a598c038eb31c019fb661377f
Oleg Trenkin <o.trenkin@ya.ru>
2024-02-06 14:44:19 +0300

    added initial contract

--- packages/frontend/app/provider/defaultProvider.ts
+++
@@ -1,6 +0,0 @@
-import { AlchemyProvider } from "ethers";
-import { InfuraProvider, JsonRpcProvider } from "ethers/providers";
-
-const defaultProvider = new InfuraProvider("matic-mumbai");
-
-export default defaultProvider;
